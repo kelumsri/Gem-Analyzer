d7eb56ac5482d5498b17694ee6d5a9141b7f5262
kelumsri <121974398+kelumsri@users.noreply.github.com>
2024-02-18 18:58:52 +0530

    Analysis page
    the basic components were made so that the functionality work

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landingpage from "./page/Landingpage";
+import ImageUploader from "./page/ImageUploader";
 
 function App() {
     return (
         <div class="">
-           <Router>
-            <Routes>
-                <Route path="/" element={<Landingpage/>}/>
-            </Routes>
-           </Router>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Landingpage />} />
+                    <Route path="/imageuploader" element={<ImageUploader />} />
+                </Routes>
+            </Router>
         </div>
     );
 }
--- client/src/components/Topnav.jsx
+++ client/src/components/Topnav.jsx
@@ -54,7 +54,7 @@ function Topnav() {
                                     href="/"
                                     class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
                                 >
-                                    Aboutus
+                                    About Us
                                 </a>
                             </li>
                             <li>
